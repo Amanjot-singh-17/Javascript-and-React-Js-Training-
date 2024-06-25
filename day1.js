72c76f66fe6146aad9364c41675d3fa24519869f
Amanjot-singh-17 <amanjotsingh0917@gmail.com>
2024-06-25 22:25:17 +0530

    Add files via upload
    #INTRODUCTION LECTURE
        - DATA TYPES 
         -CREATION OF VARIABLES USING
            :LET 
            :VAR
            :CONST

---
+++ day1.js
@@ -0,0 +1,73 @@
+// the introduction was discussed in the theory lecture
+//the coding part is present in this file 
+
+//the output can be generated in the following format:
+//using console.log
+console.log("first code as if hello world");
+console.log(4);
+console.log(2.14);
+
+
+//the javascript variables can be created with the help of following keywords:
+//const
+//let
+//var
+ var name="amanjot singh";
+ console.log(name);
+ 
+ //it has redefination property
+ name=5;
+ console.log(name);
+ 
+ //reassignment is possible
+ var name="robina dogra is my teacher";
+ console.log(name);
+
+ 
+ //the let variable
+ let assign="let variable created";
+ console.log(assign);
+
+ //it cant be redefined and throws error
+ //let assign=1;      //error
+ console.log(assign);
+
+ //reassignment
+ assign=1234567;
+ console.log(assign);
+ // its best practice to use let as compared to var
+
+
+ //the const variable
+ //used where constant variable is created
+ const pi=3.14;
+ console.log(pi);
+
+ //redefination and reassignment is not possible 
+ //pi=4;                             error
+ //const pi=3;                        error
+
+
+
+ // the following are data types in javascript
+ //number
+ //string
+//boolean
+//undefined
+//null or object
+//bigint
+//symbol
+ let s=4;
+ //the typeof() function is used to find the data type of the variable
+ console.log(typeof(s));
+ s="amanjot singh";
+ console.log(typeof(s));
+s=true;
+console.log(typeof(s));
+
+let p;
+console.log(typeof(p));
+p=null;
+console.log(typeof(p));
+
+
